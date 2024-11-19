c7a805963ddb082a93834376de82972af2056d9e
Mohammed Samee <86250719+samhk90@users.noreply.github.com>
2024-11-19 17:50:33 +0530

    Update HistoricalDataReport.js

--- src/components/HistoricalDataReport.js
+++ src/components/HistoricalDataReport.js
@@ -7,7 +7,7 @@ import html2canvas from 'html2canvas';
 
 // Initialize Supabase Client
 // Initialize Supabase client
-const supabase = createClient('https://wxundvfcpvhhggpifltc.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind4dW5kdmZjcHZoaGdncGlmbHRjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjY5MDU3MDgsImV4cCI6MjA0MjQ4MTcwOH0.uFy5Vkgbh0vdPAwux4VlFdDs8VNqEiY5fUgWns_UOCM');
+const supabase = createClient('https://wxundvfcpvhhggpifltc.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRkcWVjeGlzY3FjeXhoZ3B2cGZpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzIwMTQyNTksImV4cCI6MjA0NzU5MDI1OX0.jfwVI2qu6oBRbCwvN6OnaIp3JvubPAi14YRf0RQFugI');
 
 
 export default function HistoricalDataReport() {
